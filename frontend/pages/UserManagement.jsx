853a77781cdda4671487c297b528f188291191e6
naveenv0906 <naveenv0906@gmail.com>
2026-04-18 13:49:17 +0530

    admin role and permission updated

--- frontend/pages/UserManagement.jsx
+++ frontend/pages/UserManagement.jsx
@@ -132,6 +132,7 @@ const UserManagement = () => {
             >
               <option value="USER">User</option>
               <option value="SUPER_ADMIN">Super Admin</option>
+              <option value="ADMIN">Admin</option>
               <option value="ENQUIRY">Enquiry</option>
               <option value="ACCOUNT">Account</option>
             </select>
